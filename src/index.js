d7f5540d01b5f1d9336621ed223d772c6cb8fe41
Ahmed Saad <ahmed.saad.kader@gmail.com>
2022-05-08 17:12:42 +0200

    functionality

--- src/index.js
+++ src/index.js
@@ -3,34 +3,55 @@ import ToDoItem from './to_do_item';
 import ToDoList from './to_do_list';
 
 // defaults
-const defaultToDoList = new ToDoList('uncategorized', 'uncategorized')
+const personalToDoList = new ToDoList('Personal', 'Personal')
+const workToDoList = new ToDoList('Work', 'Work')
+const toDoCategories = []
+const allTasks = []
 
 // Selectors
 const newTaskButtons = document.querySelectorAll('.new-task-button')
 const newListButtons = document.querySelectorAll('.new-list-button')
 const taskModal = document.getElementById('taskModal')
+const listModal = document.getElementById('list-modal')
 const taskFormElement = document.getElementById('task-form')
+const listFormElement = document.getElementById('list-form')
 const saveNewTaskButton = document.getElementById('save-task')
 const closeNewTaskButton = document.getElementById('close-task-modal')
+const saveNewlistButton = document.getElementById('save-list')
+const closeNewlistButton = document.getElementById('close-list')
 const taskLists = document.getElementById('to-do-lists')
 const allTasksList = document.getElementById('all-tasks-list')
 const newTaskTitle = document.getElementById('task-title')
 const newTaskDescription = document.getElementById('task-description')
+const newTaskCategory = document.getElementById('task-category')
 const newTaskDate = document.getElementById('task-date')
 const newTaskPriority = document.getElementById('task-priority')
+const newListTitle = document.getElementById('list-title')
+const newListDescription = document.getElementById('list-description')
+
 // Event Listeners
 window.addEventListener('load', pageLoad)
 newTaskButtons.forEach((button)=>{button.addEventListener('click', openTaskModal)})
-newListButtons.forEach((button)=>{button.addEventListener('click', (e)=>{addToDoList(e)})})
 saveNewTaskButton.addEventListener('click', addToDoTask)
 closeNewTaskButton.addEventListener('click', (e)=>{closeTaskModal(e)})
+newListButtons.forEach((button)=>{button.addEventListener('click', openListModal)})
+saveNewlistButton.addEventListener('click', addToDoList)
+closeNewlistButton.addEventListener('click', (e)=>{closelistModal(e)})
+
+
+
 // Functions
 function pageLoad() {
-  addListToTaskLists(defaultToDoList)
+  addListToTaskLists(personalToDoList)
+  addListToTaskLists(workToDoList)
 }
 
 function openTaskModal() {
   taskModal.showModal()
+}
+
+function openListModal() {
+  listModal.showModal()
 }
 
 function closeTaskModal(e) {
@@ -39,26 +60,38 @@ function closeTaskModal(e) {
   taskModal.close()
 }
 
-function addToDoTask(e) {
-  // e.preventDefault()
+function closelistModal(e) {
+  e.preventDefault()
+  listFormElement.reset()
+  listModal.close()
+}
+
+function addToDoTask() {
   if (newTaskTitle.value === '') {return false}
   const newTask = new ToDoItem(newTaskTitle.value, newTaskDescription.value, newTaskDate.value, newTaskPriority.value)
   newTask.checkDueDate()
-  defaultToDoList.addTask(newTask)
+  addTaskToCategory(newTask)
+  allTasks.push(newTask)
   addTaskToAllTasksList(newTask)
   taskFormElement.reset()
   taskModal.close()
 }
 
 function addToDoList() {
-  const newList = new ToDoList('new List', 'new')
+  if (newListTitle.value === '') {return false}
+  const newList = new ToDoList(newListTitle.value, newListDescription.value)
   addListToTaskLists(newList)
+  listFormElement.reset()
+  listModal.close()
+  document.getElementById('search').focus()
 }
 
 function addListToTaskLists(list) {
   const newListElement = document.createElement('div')
+  toDoCategories.push(list)
   newListElement.innerText = list.title
   taskLists.appendChild(newListElement)
+  addListToCategoryDOM(list)
 }
 
 function addTaskToAllTasksList(task){
@@ -68,12 +101,13 @@ function addTaskToAllTasksList(task){
   addTaskTitleToList(newTaskElement, task)
   addTaskPriorityToList(newTaskElement, task)
   addTaskDateToList(newTaskElement, task)
+  checkIfTaskIsDone(task)
 }
 
 function addTaskTitleToList(div, task) {
   const taskInput = document.createElement('input')
   taskInput.setAttribute('type', 'checkbox')
-  taskInput.setAttribute('id', task.title)
+  taskInput.classList = 'title-checkbox'
   taskInput.setAttribute('name', task.title)
   const taskInputLabel = document.createElement('label')
   taskInputLabel.setAttribute('for', task.title)
@@ -99,4 +133,31 @@ function addTaskPriorityToList(div, task) {
 
   task.priority == 'high' ? div.classList.add('high'):
   task.priority == 'medium' ? div.classList.add('medium') : div.classList.add('low')
+}
+
+function addTaskToCategory(task) {
+  toDoCategories.forEach((category) => {
+    if (category.title === newTaskCategory.value){
+      category.addTask(task)
+    }
+  })
+}
+
+function checkIfTaskIsDone(task) {
+  const tasksCheckboxes = document.querySelectorAll('.title-checkbox')
+  tasksCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener('click', ()=>{
+     checkbox.parentNode.classList.toggle('task-done')
+     task.checkBoxClick()
+     console.log(task)
+    })
+  })
+}
+
+function addListToCategoryDOM (list) {
+  const categoryOption = document.createElement('option')
+  categoryOption.setAttribute('value', list.title)
+  categoryOption.innerText = list.title
+  if(list.title == "Personal"){categoryOption.selected = true}
+  newTaskCategory.appendChild(categoryOption)
 }
--- src/to_do_item.js
+++ src/to_do_item.js
@@ -1,21 +1,28 @@
 export default class ToDoItem {
-    constructor (title, description, dueDate, priority) {
+    constructor (title, description, dueDate, priority, done=false) {
         this.title = title
         this.description = description
         this.dueDate = dueDate
         this.priority = priority
+        this.done = done
     }
 
     checkDueDate() {
         let taskDueDate = this.dueDate
         var nowDate = new Date(); 
         var date = nowDate.getFullYear()+'/'+(nowDate.getMonth()+1)+'/'+nowDate.getDate(); 
-        taskDueDate === '' ? taskDueDate = date: console.log('bye')
+        if(taskDueDate === ''){
+            taskDueDate = date
+        } 
         this.dueDate = taskDueDate
     }
 
-    test() {
-        console.log(this.title, this.description, this.dueDate)
+    checkBoxClick() {
+        if (this.done == false) {
+            this.done = true
+        } else{
+            this.done = false
+        }
     }
     
 }
